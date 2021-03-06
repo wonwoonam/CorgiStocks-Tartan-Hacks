f93af4e6719f150bc1ec41cd654e5a73d82f1305
Won Woo Nam <wwnspider@gmail.com>
2021-03-06 02:16:02 -0500

    Initial Commit

---
+++ corgistocks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  corgistocks
+//
+//  Created by Won Woo Nam on 3/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
